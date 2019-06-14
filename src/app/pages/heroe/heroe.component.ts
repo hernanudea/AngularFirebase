5ec0f3a2ff7462f7fdc445f68a0cdbb41a348fd2
Hernan Velasquez <hernanudea@gmail.com>
2019-06-14 10:28:30 -0500

    se crean componentes HTML y CSS, se incluye fontawesome y animated.css

---
+++ src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import { HeroeModel } from "src/app/models/heroe.model";
+
+@Component({
+  selector: "app-heroe",
+  templateUrl: "./heroe.component.html",
+  styleUrls: ["./heroe.component.css"]
+})
+export class HeroeComponent implements OnInit {
+
+  heroe = new HeroeModel();
+
+  constructor() {}
+
+  ngOnInit() {}
+}
